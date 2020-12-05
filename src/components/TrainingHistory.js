79452eea627fe50adaa0db0874b9519a84574623
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-12-05 15:10:18 +0300

    first commit

---
+++ src/components/TrainingHistory.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import moment from "moment";
+import shortid from "shortid";
+import TrainingAddForm from "./TrainingAddForm.js";
+import trainingsAddChange from "./trainingsAddChange.js";
+import TrainingsList from "./TrainingsList.js";
+import TrainingModel from "../model/TrainingModel.js";
+
+export default function TrainingHistory() {
+  const [trainings, setTrainings] = useState([]);
+  const [itemTraining, setItemTraining] = useState({
+    id: "",
+    date: "",
+    distance: "",
+  });
+
+  const sortTrainings = trainings.sort(
+    (a, b) => moment(b.date, "DD.MM.YY") - moment(a.date, "DD.MM.YY")
+  );
+
+  const handleFormSubmit = (objValue) => {
+    const { id, date, distance, kilometr } = objValue;
+
+    if (!id) {
+      const training = new TrainingModel(
+        shortid.generate(),
+        date,
+        distance,
+        kilometr
+      );
+
+      setTrainings([...trainingsAddChange(trainings, training)]);
+    } else {
+      setTrainings((prevTrainings) =>
+        prevTrainings.map((itemTrainings) => {
+          if (itemTrainings.id === id) {
+            setItemTraining({
+              id: "",
+              date: "",
+              distance: "",
+            });
+            return new TrainingModel(id, date, distance, kilometr);
+          }
+          return itemTrainings;
+        })
+      );
+    }
+  };
+
+  const handleRemove = (id) => {
+    setTrainings((prevTrainings) => prevTrainings.filter((o) => o.id !== id));
+  };
+
+  const handleChange = (objValue) => {
+    setItemTraining({
+      id: objValue.id,
+      date: objValue.date,
+      distance: `${objValue.distance}${objValue.kilometr}`,
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <TrainingAddForm
+        valueForm={itemTraining}
+        // onFormChangre={handleFormChange}
+        onFormSubmit={handleFormSubmit}
+      />
+      <TrainingsList
+        data={sortTrainings}
+        onRemove={handleRemove}
+        onChange={handleChange}
+      />
+    </React.Fragment>
+  );
+}
---
+++ src/components/TrainingsList.js
@@ -0,0 +1,64 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TrainingModel from "../model/TrainingModel.js";
+
+export default function TrainingsList(props) {
+  const { data } = props;
+
+  const onRemove = (id) => {
+    props.onRemove(id);
+  };
+
+  const onChange = (objValue) => {
+    props.onChange(objValue);
+  };
+
+  return (
+    <React.Fragment>
+      <table>
+        <thead>
+          <tr>
+            <td>Дата (ДД.ММ.ГГ)</td>
+            <td>Пройдено км</td>
+            <td>Действия</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((o) => (
+            <tr key={o.id}>
+              <td>{o.date}</td>
+              <td>
+                {o.distance}
+                {o.kilometr}
+              </td>
+              <td>
+                <span
+                  className="change"
+                  onClick={() =>
+                    onChange({
+                      id: o.id,
+                      date: o.date,
+                      distance: o.distance,
+                      kilometr: o.kilometr,
+                    })
+                  }
+                >
+                  {" "}
+                </span>
+                <span className="remove" onClick={() => onRemove(o.id)}>
+                  {" "}
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
+}
+
+TrainingsList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.instanceOf(TrainingModel)).isRequired,
+  onRemove: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
